c1d7136cb196ef150b7ec8586edf7f6440145386
Corey Marchand <corey.marchand@me.com>
2019-11-19 10:29:28 -0800

    counter working

--- src/app.js
+++ src/app.js
@@ -1,49 +1,60 @@
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import Footer from './footer';
+import Header from './header';
+import './app.scss';
 
-const Header = () => {
-  return (
-    <header>
-      <h1>Header</h1>
-    </header>
-  );
-};
+// const Header = () => {
+//   return (
+//     <header>
+//       <h1>Header</h1>
+//     </header>
+//   );
+// };
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018 Code Fellows</footer>;
-  }
-}
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018 Code Fellows</footer>;
+//   }
+// }
 
-class Main extends React.Component {
+class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      stuff: 0,
+      counter: 0,
     };
   }
 
   handleButtonClick = e => {
     e.preventDefault();
-    this.setState({ stuff: Math.floor(Math.random() * 20 + 1) });
+    this.setState({ counter: this.state.counter +1});
+  };
+
+  secondHandleButtonClick = e => {
+    e.preventDefault();
+    this.setState({ counter: this.state.counter -1});
   };
 
   render() {
     return (
       <div>
-        <h4>{this.state.stuff}</h4>
-        <button onClick={this.handleButtonClick}>Click Me</button>
+
+        <h4>{this.state.counter}</h4>
+        <button id="button1" onClick={this.handleButtonClick}>Click Me</button>
+        <button id="button2" onClick={this.secondHandleButtonClick}>Dont Click Me</button>
       </div>
     );
   }
 }
+
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Header />
-        <Main />
+        <Counter />
         <Footer />
       </React.Fragment>
     );
---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import App from './app';
+
+
+class Footer extends React.Component {
+  render() {
+    return <footer>&copy; 2018 Code Fellows</footer>;
+  }
+}
+
+export default Footer;
